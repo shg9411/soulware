1d16dcedbd4791df686136c6eead0705fe1fb02a
shg9411 <shg9411@naver.com>
2021-03-30 20:24:22 +0900

    local

--- web_proj/accounts/urls.py
+++ web_proj/accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.CreateAPI.as_view()),
-    path('login/', views.LoginAPI.as_view()),
+    path('create', views.CreateAPI.as_view()),
+    path('login', views.LoginAPI.as_view()),
 ]
